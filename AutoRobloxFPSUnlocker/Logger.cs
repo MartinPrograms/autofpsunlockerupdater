726fb06bf509592ca61e91e0c847deccfee892e8
MartinPrograms <martinrages@gmail.com>
2023-07-29 03:28:19 +0200

    Start existing
    haha

---
+++ AutoRobloxFPSUnlocker/Logger.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRobloxFPSUnlocker
+{
+    public class Logger
+    {
+        public static List<LogEvent> LogEvents = new List<LogEvent>();
+
+        public static void LogMessage(string message)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Info, message, null));
+            Console.WriteLine(message);
+        }
+        public static void LogInfo(string message)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Info, message, null));
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogWarning(string message)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Warning, message, null));
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogError(string message, Exception exception)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Error, message, exception));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogCritical(string message, Exception exception)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Critical, message, exception));
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogError(string message)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Error, message, null));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogCritical(string message)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Critical, message, null));
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void LogException(Exception exception)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Error, exception.Message, exception));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(exception.Message);
+            Console.ResetColor();
+        }
+
+        public static void SaveLog()
+        {
+            // Save log to file
+            StringBuilder sb = new StringBuilder();
+            foreach (var log in LogEvents)
+            {
+                sb.AppendLine($"[{log.Time}] {log.Severity.ToString().ToUpper()}: {log.Message}");
+                if (log.Exception != null)
+                {
+                    sb.AppendLine($"Exception: {log.Exception.Message}");
+                    sb.AppendLine($"Stack Trace: {log.StackTrace}");
+                }
+            }
+
+            System.IO.File.WriteAllText("log.txt", sb.ToString());
+        }
+
+        public static void LogSuccess(string v)
+        {
+            LogEvents.Add(new LogEvent(LogSeverity.Info, v, null));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(v);
+            Console.ResetColor();
+        }
+    }
+
+    public class LogEvent
+    {
+        public DateTime Time { get; set; }
+        public LogSeverity Severity { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+        public string StackTrace { get; set; }
+
+        public LogEvent(LogSeverity severity, string message, Exception exception)
+        {
+            Time = DateTime.Now;
+            Severity = severity;
+            Message = message;
+
+            if (exception != null)
+            {
+                Exception = exception;
+                StackTrace = exception.StackTrace;
+            }
+        }
+    }
+
+    public enum LogSeverity
+    {
+        Info,
+        Warning,
+        Error,
+        Critical,
+    }
+
+}
---
+++ AutoRobloxFPSUnlocker/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Diagnostics;
+using System.Net;
+using Octokit;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+
+namespace AutoRobloxFPSUnlocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Config.Default.InStartup)
+            {
+                Logger.LogInfo("Do you want to add the program to startup Y/N?");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "y")
+                {
+                    Config.Default.InStartup = true;
+                    Config.Default.Save();
+                    Logger.LogInfo("Added to startup!");
+                    AddToStartup();
+                }
+                else
+                {
+                    Config.Default.InStartup = false;
+                    Config.Default.Save();
+                    Logger.LogInfo("Not added to startup!");
+                }
+            }
+            if (args.Length > 0)
+            {
+                if (args[0] == "--self")
+                {
+                    UpdateSelf();
+                }
+
+                if (args[0] == "--info")
+                {
+                    Logger.LogInfo("AutoRobloxFPSUnlocker");
+                    Logger.LogSuccess("Made by mart111n (discord)");
+                    Logger.LogSuccess("Mostly for convenience, actual effort by axstin (github)");
+
+                    Console.WriteLine();
+                    Logger.LogCritical($"Version Installed: {Config.Default.InstalledVersion}");
+                    Logger.LogCritical($"In Startup: {Config.Default.InStartup}");
+                    Logger.LogInfo("Press any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                new FpsUnlocker().Start();
+            }
+        }
+
+        private static void AddToStartup()
+        {
+            // Add a batch file to startup that runs the program
+            var a = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            var batchFile = Path.Combine(a, "AutoRobloxFPSUnlocker.bat");
+            File.WriteAllText(batchFile, $"\"{Path.Combine(Environment.CurrentDirectory, "AutoRobloxFPSUnlocker.exe")}\"");
+        }
+
+        internal static void UpdateSelf()
+        {
+
+        }
+    }
+
+    internal class FpsUnlocker
+    {
+        public void Start()
+        {
+            Logger.LogInfo("Starting...");
+            Logger.LogInfo("Checking for updates...");
+            CheckForUpdates();
+            Logger.LogInfo("Checking for updates... Done!");
+            Logger.LogInfo("Starting FPS Unlocker...");
+            StartFpsUnlocker();
+        }
+
+        private void StartFpsUnlocker()
+        {
+            Process.Start("rbxfpsunlocker.exe");
+        }
+
+        private void CheckForUpdates()
+        {
+            if (!File.Exists("rbxfpsunlocker.exe"))
+            {
+                UpdateFpsUnlocker();
+            }
+            else
+            {
+                var currentVersion = Config.Default.InstalledVersion;
+                var latestVersion = Convert.ToInt32(GetLatestVersion().Replace("v", "").Replace(".", ""));
+                if (currentVersion != latestVersion)
+                {
+                    Logger.LogInfo("New version available!");
+                    Logger.LogInfo($"Current version: {currentVersion}");
+                    Logger.LogInfo($"Latest version: {latestVersion}");
+                    Logger.LogInfo("Updating...");
+                    UpdateFpsUnlocker();
+                }
+                else
+                {
+                    Logger.LogInfo("No updates available!");
+                }
+            }
+        }
+
+        private void UpdateFpsUnlocker()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("AutoRobloxFPSUnlocker"));
+            var releases = client.Repository.Release.GetAll("axstin", "rbxfpsunlocker").Result;
+            var latestRelease = releases[0];
+            var assets = latestRelease.Assets;
+            var asset = assets[0];
+            var downloadUrl = asset.BrowserDownloadUrl;
+            var fileName = asset.Name;
+            var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+            var webClient = new WebClient();
+            webClient.DownloadFile(downloadUrl, filePath);
+            Logger.LogInfo("Updated!");
+            Config.Default.InstalledVersion = Convert.ToInt32(GetLatestVersion().Replace("v", "").Replace(".", ""));
+            Config.Default.Save();
+            var a = ZipArchive.Open(fileName, new ReaderOptions());
+            a.WriteToDirectory(Environment.CurrentDirectory, new ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+            a.Dispose();
+            File.Delete(fileName);
+            Logger.LogSuccess("Updated & Extracted!");
+        }
+
+        private string GetLatestVersion()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("AutoRobloxFPSUnlocker"));
+            var releases = client.Repository.Release.GetAll("axstin", "rbxfpsunlocker").Result;
+            var latestRelease = releases[0];
+            return latestRelease.TagName;
+        }
+    }
+}
